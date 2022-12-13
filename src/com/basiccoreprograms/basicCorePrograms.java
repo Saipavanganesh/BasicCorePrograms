ebdaf667375e34e7a91afaba6d370886b1d98846
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-14 04:30:58 +0530

    Largest of 3 numbers

--- src/com/basiccoreprograms/basicCorePrograms.java
+++ src/com/basiccoreprograms/basicCorePrograms.java
@@ -1,36 +1,26 @@
 package com.basiccoreprograms;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class basicCorePrograms {
     public static void main(String[] args) {
-        System.out.println("Vowel or Consonant");
-        System.out.println("******************");
+        System.out.println("Largest of 3 numbers");
+        System.out.println("********************");
         System.out.println(" ");
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter an alphabet");
-        char ch = sc.next().charAt(0);
-        switch(ch){
-            case 'a':
-            case 'A':
-            case 'e':
-            case 'E':
-            case 'i':
-            case 'I':
-            case 'o':
-            case 'O':
-            case 'u':
-            case 'U':
-                System.out.println("Vowel");
-                break;
-            default :
-                System.out.println("Consonant");
-                break;
+        int[] numArr=new int[3];
+        for(int i=0;i<3;i++){
+            System.out.println("Enter number");
+            int num= sc.nextInt();
+            numArr[i]=num;
         }
-
+        Arrays.sort(numArr);
+        System.out.println("Largest number is: "+numArr[2]);
     }
 }
 
